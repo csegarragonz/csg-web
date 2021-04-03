7256e2512f24431b2a28d8b10fc5ddc4c9c674e3
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-04-03 17:44:22 +0100

    wip

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,11 +1,13 @@
 from invoke import Collection
 
 from . import cv
+from . import deploy
 from . import docker
 from . import jekyll
 
 ns = Collection(
     cv,
+    deploy,
     docker,
     jekyll,
 )
