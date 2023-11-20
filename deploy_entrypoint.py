f934d0b43593a95704938a78f57feab2e5366894
Carlos Segarra <carlos@carlossegarra.com>
2023-11-20 12:40:10 -0500

    csg-server: fixes for new server

--- deploy_entrypoint.py
+++ deploy_entrypoint.py
@@ -1,4 +1,36 @@
-from tasks.jekyll import run_bg_invokeless
+from subprocess import run
+from os.path import dirname, realpath
+
+PROJ_ROOT = dirname(dirname(dirname(realpath(__file__))))
+HOST_KEYS_DIR = "{}/keys".format(PROJ_ROOT)
+CLI_KEYS_DIR = "/etc/letsencrypt/live/carlossegarra.com/"
+WEB_CTR_NAME = "live-web"
+
+
+def get_docker_tag():
+    with open("VERSION", "r") as fh:
+        ver = fh.read()
+        ver = ver.strip()
+
+    return "csegarragonz/csg-website:{}".format(ver)
+
+
+def run_bg_invokeless():
+    """
+    Run the container in dettached mode
+    """
+    _docker_cmd = [
+        "docker run -d",
+        "--name {}".format(WEB_CTR_NAME),
+        "-v {}/fullchain.pem:{}/fullchain.pem".format(HOST_KEYS_DIR, CLI_KEYS_DIR),
+        "-v {}/privkey.pem:{}/privkey.pem".format(HOST_KEYS_DIR, CLI_KEYS_DIR),
+        "-p 443:443 -p 80:80",
+        get_docker_tag(),
+    ]
+    docker_cmd = " ".join(_docker_cmd)
+    print(docker_cmd)
+    run(docker_cmd, shell=True, check=True, cwd=PROJ_ROOT)
+
 
 if __name__ == "__main__":
     run_bg_invokeless()
--- tasks/deploy.py
+++ tasks/deploy.py
@@ -7,7 +7,9 @@ from tasks.util.env import PROJ_ROOT, JEKYLL_VERSION, CONTAINER_NAME
 from tasks.util.version import get_version, get_docker_tag
 
 KEY_FILENAME = "/home/csegarra/.ssh/id_rsa"
-REMOTE_DIR = "/home/carlos/csg-web"
+SERVER_IP = "163.172.177.63"
+SERVER_USERNAME = "csegarra"
+REMOTE_DIR = "/home/{}/csg-web".format(SERVER_USERNAME)
 
 
 def print_output(stream):
@@ -15,7 +17,7 @@ def print_output(stream):
         print(line.decode("utf-8"))
 
 
-def buildPush():
+def build_push():
     build_cmd = "inv docker.build --nocache --push"
     print(build_cmd)
     run(build_cmd, shell=True, check=True, cwd=PROJ_ROOT)
@@ -24,15 +26,15 @@ def buildPush():
 @task(default=True)
 def deploy(ctx):
     """
-    Deploy the remote web
+    Deploy the website on the remote server
     """
     # Build and push container
-    buildPush()
+    build_push()
 
     # Initialize the paramiko client
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname="163.172.155.43", username="carlos", key_filename=KEY_FILENAME)
+    ssh.connect(hostname=SERVER_IP, username=SERVER_USERNAME, key_filename=KEY_FILENAME)
 
     # Pull latest image
     dock_cmd = "docker pull {}".format(get_docker_tag())
@@ -40,13 +42,13 @@ def deploy(ctx):
     stdin, stdout, stderr = ssh.exec_command(dock_cmd)
     print_output(stdout)
 
-    # Stop previous docker image
-    clean_cmd = "cd {} && docker rm -f {}".format(REMOTE_DIR, CONTAINER_NAME)
+    # Prepare the remote environment
+    clean_cmd = "cd {} && git pull origin main && docker rm -f {}".format(REMOTE_DIR, REMOTE_DIR, CONTAINER_NAME)
     print("csg-paris: {}".format(clean_cmd))
     stdin, stdout, stderr = ssh.exec_command(clean_cmd)
     print_output(stdout)
 
-    # Rebuild docker image
+    # Run remote deploy script
     run_cmd = "cd {} && python3 deploy_entrypoint.py".format(REMOTE_DIR)
     print("csg-paris: {}".format(run_cmd))
     stdin, stdout, stderr = ssh.exec_command(run_cmd)
