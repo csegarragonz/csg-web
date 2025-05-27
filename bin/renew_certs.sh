a346a04ea2bf224ed170477c6dc4150eedc81c02
Carlos Segarra <carlos@carlossegarra.com>
2025-05-26 19:21:02 -0700

    bin: update renew certs script

--- bin/renew_certs.sh
+++ bin/renew_certs.sh
@@ -3,12 +3,11 @@
 ##############################################################################
 # sudo crontab to renew the TLS certificates used to host our personal website
 #
-# To configure, open the sudo crontab file and add the following entyr:
-# sudo crontab -e
-# 0 */12 * * * /home/csegarra/csg-web/bin/renew_certs.sh
+# 0 3 1 */2 * /home/csegarra/csg-web/bin/renew_certs.sh
 ##############################################################################
 
-WEB_DIR="/home/csegarra/csg-web"
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+WEB_DIR="${THIS_DIR}/.."
 
 pushd ${WEB_DIR} >> /dev/null
 
@@ -16,18 +15,18 @@ pushd ${WEB_DIR} >> /dev/null
 docker rm -f live-web
 
 # Renew certificates (install certbot if it doesn't exist)
-certbot -q renew --standalone
+sudo certbot -q renew --standalone
 
 # Copy keys into the web directory
 KEYS_DIR=${WEB_DIR}/keys
 mkdir -p ${KEYS_DIR}
-cp /etc/letsencrypt/live/carlossegarra.com/fullchain.pem ${KEYS_DIR}/fullchain.pem
-cp /etc/letsencrypt/live/carlossegarra.com/privkey.pem ${KEYS_DIR}/privkey.pem
+sudo cp /etc/letsencrypt/live/carlossegarra.com/fullchain.pem ${KEYS_DIR}/fullchain.pem
+sudo cp /etc/letsencrypt/live/carlossegarra.com/privkey.pem ${KEYS_DIR}/privkey.pem
 
 # Give the right permissions
-chown -R csegarra:csegarra ${KEYS_DIR}
+sudo chown -R csegarra:csegarra ${KEYS_DIR}
 
 # Run the container again
-su - csegarra -c "python3 ${WEB_DIR}/deploy_entrypoint.py"
+python3 ${WEB_DIR}/deploy_entrypoint.py
 
 popd >> /dev/null
